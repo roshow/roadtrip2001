b47492a2e435cee37d680a63feaa6918cdaca658
Glitch (svelte) <none>
2020-01-29 21:46:31 +0000

    Before import of sveltejs/template without specified path ./package.json:334152/7031 ./.gitignore:334152/5 ./src/app.js:334152/71

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ import App from './components/App.html';
 const app = new App({
   target: document.querySelector('main'),
   data: {
-    message: 'wowowow you did it lol'
+    message: 'this is a svelte starter app on glitch!'
   }
 })
