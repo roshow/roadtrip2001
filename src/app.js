2fd71637d0a1a2751672ceee2f101c36ff83ae18
Glitch (svelte) <none>
2020-01-29 21:46:34 +0000

    Import of sveltejs/template ./package.json:334152/7031 ./.gitignore:334152/5 ./src/app.js:334152/71

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import App from './components/App.html';
-
-const app = new App({
-  target: document.querySelector('main'),
-  data: {
-    message: 'this is a svelte starter app on glitch!'
-  }
-})
