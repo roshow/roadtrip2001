2f845fd4fc64646d528cb806c67ee7cca68a6e65
Glitch (svelte) <none>
2020-01-29 22:29:31 +0000

    🏹☄️ Checkpoint ./src/App.svelte:334152/65 ./watch.json:334152/909

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 	<h1>Hello {name}, welcome to Svelte!</h1>
 	<p><a href="https://glitch.com/edit/#!/svelte">Remix this on Glitch</a> and visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
   
-  <div class="glitchButton"></div>
+  <div class="glitchButton" style="position:absolute;top:20px;right:20px;"></div>
 </main>
 
 <style>
