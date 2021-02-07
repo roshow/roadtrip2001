1513dd2abc3e5c6ef3a2bda9aa79d092b29ed6c3
Glitch (svelte) <none>
2021-02-07 21:52:10 +0000

    🐁🚠 Checkpoint ./package.json:334152/6543 ./rollup.config.js:334152/321

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import svelte from "rollup-plugin-svelte";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
 
@@ -11,6 +13,12 @@ export default {
     file: "public/build/bundle.js"
   },
   plugins: [
+    svelte(),
+    resolve({
+      browser: true,
+      dedupe: ["svelte"]
+    }),
+    commonjs(),
     css({ output: "bundle.css" }),
     terser()
   ],
